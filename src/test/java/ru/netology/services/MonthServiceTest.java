823a17c6a9ad08be527a7cdab6c28ae8c5ce196a
Katerina Voroshilova <kotovaekaterina1@gmail.com>
2023-07-30 14:14:45 +0300

    Task 2

--- src/test/java/ru/netology/services/MonthServiceTest.java
+++ src/test/java/ru/netology/services/MonthServiceTest.java
@@ -2,13 +2,12 @@ package ru.netology.services;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
 public class MonthServiceTest {
     @ParameterizedTest
-    @CsvSource({
-            "3,10000,3000,20000",
-            "2,100000,60000,150000"
-    })
+    @CsvFileSource(files="src/test/resources/month.csv")
+
     public void test(int expected, int income, int expense, int treshold) {
         MonthService service = new MonthService();
         int actual = service.calculate(income, expense, treshold);
